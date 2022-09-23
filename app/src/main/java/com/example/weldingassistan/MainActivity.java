033949fa48e791b78db48b8865392a635f5bbfbe
Egor <egormaluh@gmail.com>
2022-09-23 18:01:52 +0300

    ПИВО

--- app/src/main/java/com/example/weldingassistan/MainActivity.java
+++ app/src/main/java/com/example/weldingassistan/MainActivity.java
@@ -2,27 +2,31 @@ package com.example.weldingassistan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button bt1;
+    private Button btnInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bt1 = findViewById(R.id.button1);
 
-        bt1.setOnClickListener(view -> {
-
-
-        });
-
-
-
-
-
+        init();
     }
-
+    private void init(){
+        btnInfo = findViewById(R.id.btnInfo);
+    }
+    private void kkk(){
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, InfoActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
